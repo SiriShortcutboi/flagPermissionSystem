24b5cfe503896b6009e97dd3fa358aae3a0c52a9
holden.anderson <holden.anderson@students.snow.edu>
2026-02-27 17:07:46 -0700

    COMPILES this save is good reference point. need to add logic for decide which menu item to select

--- flagPermissionSystem/Program.cs
+++ flagPermissionSystem/Program.cs
@@ -4,21 +4,23 @@ class Program
 {
 
     public static string userInput = "bruh";
-    public static int permissionLevel = 0;     
+    public static Permissions userPermissions;     
     public static bool mainMenuBool = true;
-    public static void main()
-        {
-        Permissions userPermissions = Permissions.None;
-        
+    public static void Main()
+    {
+        // Permissions userPermissions = Permissions.None;
+        //System.Console.WriteLine((int)userPermissions);
+
+
         ApplicationMenu();
 
-        userPermissions = Permissions.Read | Permissions.Write;
+        //userPermissions = Permissions.Read | Permissions.Write;
 
-        Console.WriteLine($"Current Permissions: {userPermissions} ");
+        //Console.WriteLine($"Current Permissions: {userPermissions} ");
 
 
         
-        } //void main end
+    } //void main end
     
 
     [Flags]
@@ -45,48 +47,102 @@ class Program
             Console.WriteLine("3. Perform Action");
             Console.WriteLine("4. Exit");
 
-            permissionLevel = int.Parse(Console.ReadLine());
+            int action = int.Parse(Console.ReadLine());
+                        //need to make this an if statement to run a choice isntead of all 3
+            // 1. RunAddPermissionMethod
+            AddPermissionMethod();
+            // 2. Display permission
+            DisplayPermissionMethod();
+            // 3. display text from permission
+             DisplayPermissionTextMethod();
+            // 4. break; - set mainMenuBool to false
             // permissionLevel = 
 
 
-            switch(Permissions(permissionLevel))
-            {
-
-                case Permissions.None:
-                    Console.WriteLine("User access denied");
-                    userPermissions = Permissions.None;
-                    break;
-                
-                case Permissions.Read:
-                    Console.WriteLine("User has Read access");
-                    userPermissions = Permissions.Read;
-                    break;
-                
-                case Permissions.Write:
-                    Console.WriteLine("User has Write access");
-                    userPermissions = Permissions.Write;
-                    break;
-                
-                case Permissions.Delete:
-                    Console.WriteLine("User has danger-zone access");
-                    userPermissions = Permissions.Write;
-                    break;
-                
-                case Permissions.Execute:
-                    Console.WriteLine("User can run any program");
-                    userPermissions = Permissions.Execute;
-                    break;
-                
-                case Permissions.ViewReports:
-                    Console.WriteLine("This user is a super-user, can even view reports");
-                    userPermissions = Permissions.ViewReports;
-                    break;
-            }
-
+  
             /* userPermissions = Permissions.Read | Permissions.Write;
 
             Console.WriteLine($"Current Permissions: {userPermissions} "); */
+        }    
+    }
+    public static void AddPermissionMethod()
+    {
+        //which permission to add
+        Permissions response = Permissions.Read;
+        switch(response)
+        {
+            case Permissions.None:                    
+                userPermissions |= Permissions.None;
+                break;                
+            case Permissions.Read:                    
+                userPermissions |= Permissions.Read;
+                break;                
+            case Permissions.Write:                    
+                userPermissions |= Permissions.Write;
+                break;                
+            case Permissions.Delete:                    
+                userPermissions |= Permissions.Write;
+                break;                
+            case Permissions.Execute:                   
+                userPermissions |= Permissions.Execute;
+                break;                
+            case Permissions.ViewReports:                    
+                userPermissions |= Permissions.ViewReports;
+                break;
         }
-        
+
+    }
+    public static void DisplayPermissionMethod()
+    {
+        System.Console.WriteLine(userPermissions2);
+    }
+    public static void DisplayPermissionTextMethod()
+    {
+                //which permission text to see
+        Permissions response = Permissions.Read;
+        switch(response)
+        {
+
+            case Permissions.None:
+                Console.WriteLine("User access denied");
+                break;                
+            case Permissions.Read:
+                Console.WriteLine("User has Read access");
+                break;                
+            case Permissions.Write:
+                Console.WriteLine("User has Write access");
+                break;                
+            case Permissions.Delete:
+                Console.WriteLine("User has danger-zone access");
+                break;                
+            case Permissions.Execute:
+                Console.WriteLine("User can run any program");
+                break;                
+            case Permissions.ViewReports:
+                Console.WriteLine("This user is a super-user, can even view reports");
+                break;
+
+            //NOTES TO IMPLEMENT: dISPLAY WORKS GOOD, checked off, copy things off that
+                // for feature building 
+                //1 Need to make sure Users can type the String to select the permission they want
+                //2 Need to add decision logic to display those different menu items
+                    //lines 1 and 2 right there important, 2 might have to be implemented first
+                    //Mr Jensen said you can use integers if you want, sounds harder though, 
+                    //Our permissions uses int and string, its not that deep
+                /*
+                //NEED TO CHANGE THE HARDCODED 2 BACK TO USER INPUT FOR ANY PERMISSION SYSTEM SELECTION
+                ==== Permission System ====
+                    1. Add Permission
+                    2. Show Current Permissions
+                    3. Perform Action
+                    4. Exit
+                    2 """entered 2, its hardcoded at about line 96  System.Console.WriteLine(userPermissions2);"""
+                    Read
+                    User has Read access 
+                                */
+            //these might be the same but i swear they are on line 102 and line 71, look at the commit.
+            //need to change the hardcoding on line 71
+        }
+
     }
 }
